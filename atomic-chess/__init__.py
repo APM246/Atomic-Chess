68176a4118cc7605c42996f97b01eed9e3a0a3ff
APM246 <50038558+APM246@users.noreply.github.com>
2021-04-11 02:44:01 +0800

    added navbar and 3 new pages

--- atomic-chess/__init__.py
+++ atomic-chess/__init__.py
@@ -1,11 +1,11 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 STATIC_FILE_DIRECTORY = "static"
 TEMPLATE_DIRECTORY = "templates"
 
 def create_app():
-    app = Flask(__name__, instance_relative_config=True, template_folder=TEMPLATE_DIRECTORY)
+    app = Flask(__name__, instance_relative_config=True, template_folder=TEMPLATE_DIRECTORY, static_folder=STATIC_FILE_DIRECTORY)
     app.config.from_mapping(
         SECRET_KEY="dev",
     )
@@ -15,5 +15,17 @@ def create_app():
     @app.route("/")
     def index():
         return render_template("index.html")
+
+    @app.route("/learn")
+    def learn():
+        return render_template("learn.html")
+    
+    @app.route("/stats")
+    def stats():
+        return render_template("stats.html")
+    
+    @app.route("/settings")
+    def settings():
+        return render_template("settings.html")
 
     return app

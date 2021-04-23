07bacad48878f372beb607f81aaed2bec303fd66
Jordan Morrison <jordan.thomas.morrison@gmail.com>
2021-04-23 10:29:03 +0800

    Fixed lesson css for mobile, added site icon, supported loading board settings on a per-user basis from server

--- app/routes.py
+++ app/routes.py
@@ -22,8 +22,31 @@ LESSON_NAME_MAP = {
     "intro": "lessons/lesson0.html"
 }
 
+def prepare_settings(settings_json):
+    # Jinja seems to just do simple text replacement (ie. Doesn't convert from python True to js true)
+    settings_copy = { **settings_json }
+    for key in settings_copy:
+        if settings_copy[key] == True:
+            settings_copy[key] = "true"
+        elif settings_copy[key] == False:
+            settings_copy[key] = "false"
+    return settings_copy
+
+# TODO: Lookup user settings in database
+def get_board_settings(user):
+    return prepare_settings({
+        "light_square_color": "#EEEEEE",
+        "dark_square_color": "#4682B4",
+        "light_square_highlight_color": "#F6F669",
+        "dark_square_highlight_color": "#BACA2B",
+        "show_move_markers": True,
+        "show_square_highlights": True,
+        "use_move_animations": True,
+        "animation_time_ms": 300,
+    })
+
 @app.route("/lessons/<string:name>")
 def lessons(name):
     if name in LESSON_NAME_MAP:
-        return render_template(LESSON_NAME_MAP[name])
+        return render_template(LESSON_NAME_MAP[name], board_settings=get_board_settings(user=None))
     abort(404)

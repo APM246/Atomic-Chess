08c7996076cc0669e272a3bfbec356fc77ae1a69
Jun <junkanglim1@gmail.com>
2021-04-24 20:39:42 +0800

    Added intro lesson to learn page

--- app/routes.py
+++ app/routes.py
@@ -6,10 +6,13 @@ from app import app
 def index():
     return render_template("index.html")
 
-lesson_names = ["Rules", "Win Conditions", "Opening Traps", "Checks", "Piece Saftey", "Kings Touching"]
+LESSON_NAMES = ["Chess", "Atomic", "Win Conditions", "Opening Traps", "Checks", "Piece Saftey", "Kings Touching"]
+LESSON_NAME_MAP = {name: "lessons/lesson{}.html".format(i) for i, name in enumerate(LESSON_NAMES)}
+
 @app.route("/learn")
 def learn():
     lesson_descriptions = [
+        "Never played chess before? This lesson will go through the basics of chess",
         "Learn the rules of Atomic Chess and how they differ from traditional chess",
         "In Atomic Chess you can win by checkmate, but can also win by blowing up the enemy king. This lesson will teach you how this effects the game",
         "White has many traps they can set in the opening, learn these traps to crush your oppenent!",
@@ -18,9 +21,9 @@ def learn():
         "Kings can touch each other in Atomic Chess! This lesson teaches you the consequences of this strange rule"]
 
     # todo: Query the database for which lessons have been completed
-    completed_lessons = [True, True, False, False, True, False]
+    completed_lessons = [True, True, True, False, False, True, False]
 
-    return render_template("learn.html", lessonNames=lesson_names, lessonDescriptions=lesson_descriptions, completedLessons=completed_lessons, zip=zip)
+    return render_template("learn.html", lessonNames=LESSON_NAMES, lessonDescriptions=lesson_descriptions, completedLessons=completed_lessons, zip=zip)
 
 @app.route("/stats")
 def stats():
@@ -29,11 +32,6 @@ def stats():
 @app.route("/settings")
 def settings():
     return render_template("settings.html")
-
-LESSON_NAME_MAP = {
-    "intro": "lessons/lesson0.html"
-}
-LESSON_NAME_MAP.update({name: "lessons/lesson{}.html".format(i) for i, name in enumerate(lesson_names, 1)})
 
 @app.route("/lessons/<string:name>")
 def lessons(name):

59d35e4dead4147082535f937d6135d3db123f29
APM246 <apm0366@gmail.com>
2021-04-26 22:46:35 +0800

    basic style

--- app/forms.py
+++ app/forms.py
@@ -4,12 +4,12 @@ from wtforms.validators import DataRequired
 
 class SignUpForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    password = StringField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     is_chess_beginner = BooleanField('Chess Beginner')
     submit = SubmitField('Register')
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    password = StringField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Sign In')

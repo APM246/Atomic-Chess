425d5d46cef7b6120449d5209710cc3b0d3d247b
APM246 <apm0366@gmail.com>
2021-04-27 00:57:05 +0800

    refactored code, logic placed in forms.js

---
+++ app/static/forms.js
@@ -0,0 +1,35 @@
+$(document).ready( 
+    function initialise() {
+        $("input[id='username']").on({
+            focus: function() {
+                if ($(this).val() == 'Username') 
+                {
+                    $(this).val('');
+                }
+            },
+
+            blur: function() {
+                if ($(this).val() == '' || $(this).val() == 'Username') 
+                {
+                    $(this).val('Username');
+                }
+            }
+        });
+
+        $("input[id='password']").on({ 
+            focus: function() {
+                if ($(this).val() == 'Password') 
+                {
+                    $(this).val('');
+                }
+            },
+
+            blur: function() {
+                if ($(this).val() == '' || $(this).val() == 'Password') 
+                {
+                    $(this).val('Password');
+                }
+            }
+        });
+    }
+);

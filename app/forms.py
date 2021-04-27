199f1713ad0ca1e8f8fd0ba8eaf9aee99da19a76
APM246 <apm0366@gmail.com>
2021-04-27 17:02:43 +0800

    changed info message and added rounded border to submit button

--- app/forms.py
+++ app/forms.py
@@ -4,13 +4,12 @@ from wtforms.validators import DataRequired
 
 username_field = StringField(default='Username', validators=[DataRequired()])
 password_field = StringField(default='Password', validators=[DataRequired()])
-is_chess_beginner_field = BooleanField('Chess beginner:')
 remember_me_field = BooleanField('Remember me:')
 
 class SignUpForm(FlaskForm):
     username = username_field
     password = password_field
-    is_chess_beginner = is_chess_beginner_field
+    is_chess_beginner = BooleanField('Chess beginner:', default=True, render_kw={'checked':''})
     remember_me = remember_me_field
     submit = SubmitField('Register')
 
